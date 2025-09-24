9bff8f9dbc22c3202b5dbf020f9e9bbe4ac1d260
Ypnose <hmocode@exo.re>
2025-09-24 21:54:16 +0200

    ldifview/main.go: display usage if no arg given and without stdin

--- ldifview/main.go
+++ ldifview/main.go
@@ -254,6 +254,11 @@ func main() {
 
 	var filename string
 	if len(args) == 0 {
+		stat, err := os.Stdin.Stat()
+		if err != nil || (stat.Mode()&os.ModeCharDevice) != 0 {
+			flag.Usage()
+			os.Exit(1)
+		}
 		filename = "-" // stdin
 	} else {
 		filename = args[0]
