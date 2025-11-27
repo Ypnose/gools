9bb3b21dde867f87ebdffdf39cf427be19167207
Ypnose <hmocode@exo.re>
2025-11-27 10:10:38 +0100

    ipinfo/main.go: update User-Agent

--- ipinfo/main.go
+++ ipinfo/main.go
@@ -23,7 +23,7 @@ func main() {
 	if err != nil {
 		die(err)
 	}
-	req.Header.Set("User-Agent", "curl/8.14.1")
+	req.Header.Set("User-Agent", "curl/8.17.0")
 
 	resp, err := client.Do(req)
 	if err != nil {
